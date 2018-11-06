84d1ceeff05fb65e856d843ce667ad8a47799ba2
joe <burnett.joe@gmail.com>
2018-11-05 20:09:47 -0500

    adding server js and the db schema/seeds

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Plain", False), ("Baconator", False), ("Double Cheeseburger", False), ("Kitchen Sink", False);
+
+SELECT * FROM burgers;
